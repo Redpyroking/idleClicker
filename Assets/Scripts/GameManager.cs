23039bba7cf982ebe4076793ef86da0895ab53a8
Nihal Shrivastav <nihalrs24@gmail.com>
2025-05-06 08:55:32 +0530

    add auto clicker

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,17 @@ public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] TMP_Text countText;
+    [SerializeField] TMP_Text autoClickText;
     int count = 0;
-
+    //how much point 1 tap generate
+    int tapValue = 1;
+    //For Auto Clicker
+    int autoClickerPurchase = 20;
+    int autoClickerLevel = 0;
+    float autoClickerLeveTimeInterval = 1f;
     public void Start()
     {
+        StartCoroutine(AutoClickerLoop());
         UpdateUI();
     }
     public void ClickAction()
@@ -21,5 +28,27 @@ public class GameManager : MonoBehaviour
     void UpdateUI()
     {
         countText.text = count.ToString();
+        autoClickText.text = $"Auto Clicker (Lv {autoClickerLevel})\nCost:{autoClickerPurchase:F0}";
     }
+
+    public void ClickOnAutoClick()
+    {
+        //purchase autoclick
+        if (count < autoClickerPurchase)
+        return;
+        count -= autoClickerPurchase;
+        autoClickerLevel += 1;
+        autoClickerPurchase *= 2;
+        UpdateUI();
+    }
+    IEnumerator AutoClickerLoop()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(autoClickerLeveTimeInterval);
+            count += autoClickerLevel * tapValue;
+            UpdateUI();
+        }
+    }
+    
 }
